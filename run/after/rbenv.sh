7d9ced16bba514d7efbdc40b5613421d77c404bc
Calvin Walzel <calvin@pixelhub.nl>
2022-01-24 13:49:47 +0100

    Install ruby 2.7.5 + 3.1.0 and default to 3.1.0

--- run/after/rbenv.sh
+++ run/after/rbenv.sh
@@ -5,6 +5,17 @@ echo "Installing ruby 2.7.5..."
 
 set -x
 rbenv install 2.7.5
-rbenv global 2.7.5
 # gem env home
+set +x
+
+echo "Installing ruby 3.1.0..."
+
+set -x
+rbenv install 3.1.0
+set +x
+
+echo "Setting global ruby version to 3.1.0..."
+
+set -x
+rbenv global 3.1.0
 set +x
