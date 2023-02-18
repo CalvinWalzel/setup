eb6f52c48f5894df1724996b97726be0753af3f2
Calvin Walzel <calvin@pixelhub.nl>
2023-02-18 11:46:28 +0100

    Disable dockutil until homebrew formula update

--- run/after/z_macos_setup.sh
+++ run/after/z_macos_setup.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 set -o errexit -o nounset
 
-echo "Updating Dock..."
+# Disabled until dockutil is updated to version 3 on homebrew
+# echo "Updating Dock..."
 
-set -x
-dockutil --remove all --no-restart
-dockutil --add /System/Applications/Launchpad.app --no-restart
-dockutil --add /Applications/Firefox.app --no-restart
-dockutil --add '/Applications/ChatMate for WhatsApp.app' --no-restart
+# set -x
+# dockutil --remove all --no-restart
+# dockutil --add /System/Applications/Launchpad.app --no-restart
+# dockutil --add /Applications/Firefox.app --no-restart
+# dockutil --add '/Applications/Canary Mail.app' --no-restart
+# dockutil --add /System/Applications/Calendar.app --no-restart
+# dockutil --add '/Applications/ChatMate for WhatsApp.app' --no-restart
 # dockutil --add '' --type spacer --section apps
-dockutil --add '/Applications/Canary Mail.app' --no-restart
-dockutil --add /System/Applications/Calendar.app --no-restart
-dockutil --add /System/Applications/Reminders.app --no-restart
-dockutil --add /System/Applications/Notes.app --no-restart
-# dockutil --add '' --type spacer  --section apps
-dockutil --add '/Applications/Visual Studio Code.app' --no-restart
-dockutil --add /Applications/iTerm.app --no-restart
-dockutil --add /Applications/Fork.app --no-restart
-dockutil --add ~/Downloads --display stack --view fan --section others # Implicitly restarts the Dock.
-set +x
+# # dockutil --add '' --type spacer  --section apps
+# dockutil --add '/Applications/Visual Studio Code.app' --no-restart
+# dockutil --add /Applications/Fork.app --no-restart
+# dockutil --add /Applications/iTerm.app --no-restart
+# dockutil --add ~/Downloads --display stack --view fan --section others # Implicitly restarts the Dock.
+# set +x
 
 echo "Disabling Homebrew analytics."
 set -x
