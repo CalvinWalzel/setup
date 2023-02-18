4db36f8d29f267be7d3afe6beabbbb3964c445b8
Calvin Walzel <calvin@pixelhub.nl>
2023-02-18 11:47:10 +0100

    Update ruby versions

--- run/after/rbenv.sh
+++ run/after/rbenv.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -o errexit -o nounset
 
-echo "Installing ruby 2.7.5..."
+echo "Installing ruby 2.7.7..."
 
 set -x
-rbenv install 2.7.5
+rbenv install 2.7.7
 # gem env home
 set +x
 
-echo "Installing ruby 3.1.0..."
+echo "Installing ruby 3.2.1..."
 
 set -x
-rbenv install 3.1.0
+rbenv install 3.2.1
 set +x
 
-echo "Setting global ruby version to 3.1.0..."
+echo "Setting global ruby version to 3.2.1..."
 
 set -x
-rbenv global 3.1.0
+rbenv global 3.2.1
 set +x
